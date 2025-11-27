bb15b1655f73e0eed02831963d31cf79a72f167a
accasadof71 <accasadof71@gmail.com>
2025-11-27 12:07:08 +0000

    Deploying to gh-pages from @ accasadof71/accasadof71.github.io@3c0d951cbc040491edb4d687df4fb60413cebd92 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "accasadof71/accasadof71.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
